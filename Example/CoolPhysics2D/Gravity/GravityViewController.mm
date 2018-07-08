d1f83f74f12eb144da381abeae3581cd670c9d03
luqyluqe <luqy.luqe@gmail.com>
2018-07-08 15:49:53 +0800

    example

--- Example/CoolPhysics2D/Gravity/GravityViewController.mm
+++ Example/CoolPhysics2D/Gravity/GravityViewController.mm
@@ -15,9 +15,30 @@
 
 @implementation GravityViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    UIGestureRecognizer *panRecog=[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+    [self.gameView addGestureRecognizer:panRecog];
+}
+    
+-(void)pan:(UIPanGestureRecognizer *)recognizer
+{
+    CGPoint translation=[recognizer translationInView:recognizer.view];
+    CGRect frame = recognizer.view.frame;
+    CGPoint origin=frame.origin;
+    origin.x+=translation.x;
+    origin.y+=translation.y;
+    frame.origin=origin;
+    recognizer.view.frame=frame;
+    [recognizer setTranslation:CGPointZero inView:recognizer.view];
+}
+
 -(CP2DGameView*)getGameView
 {
-    CP2DGameView* gameView=[[GravityView alloc] initWithFrame:self.view.frame];
+    CP2DGameView* gameView=[[GravityView alloc] initWithFrame:CGRectMake(0, 0, 2000, 2000)];
+    CGSize size=self.view.bounds.size;
+    gameView.center=CGPointMake(size.width/2, size.height/2);
     return gameView;
 }
 
