88c72187b585614ab1fe9d5519b07e465e948c84
luqyluqe <luqy.luqe@gmail.com>
2018-07-15 21:24:41 +0800

    all header files private

--- Example/Pods/Target Support Files/CoolPhysics2D/CoolPhysics2D-umbrella.h
+++ Example/Pods/Target Support Files/CoolPhysics2D/CoolPhysics2D-umbrella.h
@@ -10,35 +10,6 @@
 #endif
 #endif
 
-#import "config.h"
-#import "AttachableField.h"
-#import "BuoyancyField.h"
-#import "CentripetalGravityField.h"
-#import "Color.h"
-#import "DampingField.h"
-#import "Field.h"
-#import "GameWorld.h"
-#import "GravityField.h"
-#import "Circle.h"
-#import "MathDefines.h"
-#import "Rectangle.h"
-#import "Vector.h"
-#import "ObstacleField.h"
-#import "OpaqueParticle.h"
-#import "opaque_config.h"
-#import "Particle.h"
-#import "ParticleEmitter.h"
-#import "RetainCount.h"
-#import "retaincount_config.h"
-#import "Wave.h"
-#import "CP2DGameView.h"
-#import "CP2DGameViewController.h"
-#import "CP2DGameWorldRenderer.h"
-#import "CP2DParticleRenderer.h"
-#import "CP2DRenderer.h"
-#import "CP2DWaveRenderer.h"
-#import "timer.h"
-#import "ThreadPool.h"
 
 FOUNDATION_EXPORT double CoolPhysics2DVersionNumber;
 FOUNDATION_EXPORT const unsigned char CoolPhysics2DVersionString[];
